dff8d35c538c3f947c6e02c46b9cf9183f9f30cd
Arnab Gupta <arnabgupta1111@gmail.com>
2025-06-06 19:43:50 +0600

    feat(app): setup Express app with middleware and base test endpoint

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import os from 'os';
+
+const app: Application = express();
+const corsOptions: cors.CorsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+};
+
+//Middleware Setup
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Route Middleware
+
+// Test route
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  const currentDateTime = new Date().toISOString();
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const serverHostname = os.hostname();
+  const serverPlatform = os.platform();
+  const serverUptime = os.uptime();
+
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to the DocMeet Server',
+    version: '1.0.0',
+    clientDetails: {
+      ipAddress: clientIp,
+      accessedAt: currentDateTime,
+    },
+    serverDetails: {
+      hostname: serverHostname,
+      platform: serverPlatform,
+      uptime: `${Math.floor(serverUptime / 60 / 60)} hours ${Math.floor(
+        (serverUptime / 60) % 60,
+      )} minutes`,
+    },
+    developerContact: {
+      email: 'arnab.gupta.011@gmail.com',
+      website: 'https://arnab-gupta.netlify.app',
+    },
+  });
+});
+
+export default app;
